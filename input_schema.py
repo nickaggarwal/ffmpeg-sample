3ad1afcf97a7db5a79e45d2ac17c021cd7a48bc9
Nilesh Agarwal <nilesh@ip-192-168-1-19.ec2.internal>
2025-01-23 14:58:01 -0800

    fix

--- input_schema.py
+++ input_schema.py
@@ -1,8 +1,8 @@
 INPUT_SCHEMA = {
-    "prompt": {
+    "video_file": {
         'datatype': 'STRING',
         'required': True,
         'shape': [1],
-        'example': ["There is a fine house in the forest"]
+        'example': ["https://file-examples.com/storage/fe03833ad667927ba970cf0/2017/04/file_example_MP4_640_3MG.mp4"]
     }
 }
